7dab8909e6ade16e2f9cd46fa7e6ddb56593af1f
Koval Slava <darkmage272@gmail.com>
2021-11-18 18:45:07 +0300

    add function sum

---
+++ main/funcs.cpp
@@ -0,0 +1,6 @@
+#include "funcs.h"
+
+double funcSum(double a, double b)
+{
+	return a + b;
+}
---
+++ main/funcs.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef __FUNCS_H__ 
+#define __FUNCS_H__ 
+
+double funcSum(double, double);
+
+#endif // __FUNCS_H
